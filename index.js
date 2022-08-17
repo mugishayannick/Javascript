29d9b5728bef4f07c1c2ff998dfde83413896b3d
Yannick <admin@Admins-MacBook-Pro.local>
2022-08-17 15:40:34 +0200

    build a counter app

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const countEl = document.getElementById("count-el")
+const saveEl = document.getElementById("save-el")
+let count = 0
+
+
+function increment() {
+    count += 1
+    countEl.innerHTML = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent = "Previous Entries:" + " " +countStr
+    countEl.textContent = 0
+}
